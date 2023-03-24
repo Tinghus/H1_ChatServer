937844b35747408c0de3484029d229535741083f
Tinghus <m.tinghus@gmail.com>
2023-03-24 05:36:47 +0100

    Add project files.

---
+++ H1_ChatServer/Connection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ChatServer
+{
+    public class Connection
+    {
+
+        public IPAddress Ip { get; private set; }
+        public int Port { get; private set; }
+        public bool Connected { get; set; }
+        private TcpListener tcpListener { get; set; }
+        public Socket sockeForclient { get; private set; }
+
+        public NetworkStream networkStream { get; set; }
+        public StreamReader streamReader { get; set; }
+        public StreamWriter streamWriter { get; set; }
+
+
+
+        public Connection(IPAddress ip, int port)
+        {
+            Connected = true;
+            this.Ip = ip;
+            this.Port = port;
+        }
+
+        public void ListenForConnections()
+        {
+            tcpListener = new TcpListener(Ip, Port);
+            tcpListener.Start();
+        }
+
+        public void ConnectToClient()
+        {
+            try
+            {
+                sockeForclient = tcpListener.AcceptSocket();
+            }
+            catch
+            {
+                Console.WriteLine("Connection error");
+            }
+        }
+
+        public void ClientConnection()
+        {
+            networkStream = new NetworkStream(sockeForclient);
+            streamReader = new StreamReader(networkStream);
+            streamWriter = new StreamWriter(networkStream);
+        }
+
+
+        public void disconect()
+        {
+            streamWriter.Flush();
+            networkStream.Close();
+            streamReader.Close();
+            streamWriter.Close();
+            sockeForclient.Close();
+        }
+    }
+}
---
+++ H1_ChatServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+
+namespace H1_ChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            Int32 port = 8000;
+            Connection server = new Connection(ipAddress, port);
+
+            server.ListenForConnections();
+            server.ConnectToClient();
+
+            Console.WriteLine("Connection established");
+
+
+            string messageSent = "";
+            string messageRecieved = "";
+
+
+            try
+            {
+                server.ClientConnection();
+
+                while (server.Connected)
+                {
+
+                    if (server.sockeForclient.Connected)
+                    {
+                        messageRecieved = server.streamReader.ReadLine();
+                        Console.WriteLine(messageRecieved);
+
+                        if (messageRecieved == "exit")
+                        {
+                            server.Connected = false;
+                            server.streamReader.Close();
+                            server.streamWriter.Close();
+                            server.networkStream.Close();
+                            return;
+                        }
+
+                        Console.Write("Me : ");
+                        messageSent = Console.ReadLine();
+                        server.streamWriter.WriteLine(messageSent);
+                        server.streamWriter.Flush();
+                    }
+                }
+                server.disconect();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
